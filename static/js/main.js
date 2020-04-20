dd28fa03035a236f9b398550d2a9a11c76dcfa0d
Ari Nugraha <nugrahaa878@gmail.com>
2020-04-20 17:31:56 +0700

    add galeri and fix header in content

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,13 @@
-
 $(document).ready(function () {
-    $('.carousel').carousel({
-        interval: false
-    })
+    // $('.carousel').carousel({
+    //     interval: false
+    // })
+
+    $("#mcf-button").click(function(){
+        $("#mcf-more").toggle(1000);
+    });
+
+    $("#kgtk-button").click(function(){
+        $("#kgtk-more").toggle(1000);
+    });
 });
